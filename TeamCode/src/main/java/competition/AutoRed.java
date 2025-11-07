14c1f9bb26d0e9078886c6b03c36ebbca59bc37c
Art Lamb <artlamb@yahoo.com>
2025-11-07 11:30:37 -0800

    Initial Version

---
+++ TeamCode/src/main/java/competition/AutoRed.java
@@ -0,0 +1,46 @@
+package competition;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import common.Auto;
+import common.Logger;
+import utils.PoseData;
+
+@Autonomous(name="Auto Red", group="Competition")
+@com.acmerobotics.dashboard.config.Config
+
+public class AutoRed extends LinearOpMode {
+
+    public static PoseData START =      new PoseData(15.0, 62.0, 90.0);
+    public static PoseData WAYPOINT_1 = new PoseData(12,   12,   135.0);
+    public static PoseData PARK =       new PoseData(23.5, 23.5, 0.0);
+
+    Auto auto;
+
+    @Override
+    public void runOpMode() {
+
+        try {
+            initialize();
+
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+            auto.runAuto();
+
+        } catch (Exception e) {
+            Logger.error(e, "Exception");
+            throw e;
+        }
+    }
+
+    private void initialize() {
+        auto = new Auto(this);
+
+        auto.setStartPose(START.x, START.y, START.h);
+        auto.addPath(Auto.PathState.WAYPOINT_1, WAYPOINT_1.x, WAYPOINT_1.y, WAYPOINT_1.h);
+        auto.addPath(Auto.PathState.PARK, PARK.x, PARK.y, PARK.h);
+    }
+}
---
+++ TeamCode/src/main/java/competition/AutoRedGoal.java
@@ -0,0 +1,46 @@
+package competition;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import common.Auto;
+import common.Logger;
+import utils.PoseData;
+
+@Autonomous(name="Auto Red Goal", group="Competition")
+@com.acmerobotics.dashboard.config.Config
+
+public class AutoRedGoal extends LinearOpMode {
+
+    public static PoseData START =      new PoseData(47, 47, 135.0);
+    public static PoseData WAYPOINT_1 = new PoseData(12, 12, 135.0);
+    public static PoseData PARK =       new PoseData(47, 47,   0.0);
+
+    Auto auto;
+
+    @Override
+    public void runOpMode() {
+
+        try {
+            initialize();
+
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+            auto.runAuto();
+
+        } catch (Exception e) {
+            Logger.error(e, "Exception");
+            throw e;
+        }
+    }
+
+    private void initialize() {
+        auto = new Auto(this);
+
+        auto.setStartPose(START.x, START.y, START.h);
+        auto.addPath(Auto.PathState.WAYPOINT_1, WAYPOINT_1.x, WAYPOINT_1.y, WAYPOINT_1.h);
+        auto.addPath(Auto.PathState.PARK, PARK.x, PARK.y, PARK.h);
+    }
+}
