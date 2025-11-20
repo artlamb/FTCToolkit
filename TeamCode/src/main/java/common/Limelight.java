a538e17e28877b0c8c005b39ebece5e9fa0862ff
Art Lamb <artlamb@yahoo.com>
2025-11-20 09:17:14 -0800

    Removed warning

--- TeamCode/src/main/java/common/Limelight.java
+++ TeamCode/src/main/java/common/Limelight.java
@@ -7,8 +7,6 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
@@ -124,7 +122,7 @@ public class Limelight {
             }
         }
 
-        Logger.warning("No April Tag found");
+        //Logger.warning("No April Tag found");
         return 0;
     }
 
