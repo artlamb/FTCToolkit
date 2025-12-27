a7fa593a2ff7050db140fc978e402e8333731319
Art Lamb <artlamb@yahoo.com>
2025-12-27 14:35:39 -0800

    Initial version

---
+++ TeamCode/src/main/java/common/Floodgate.java
@@ -0,0 +1,26 @@
+package common;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Floodgate {
+
+    AnalogInput currentSensor;
+
+    public Floodgate(LinearOpMode opMode) {
+        currentSensor = opMode.hardwareMap.get(AnalogInput.class, "currentSensor");
+
+    }
+
+    /**
+     * Get the current that the robot is consuming.
+     */
+    public double getCurrent() {
+        double maxVoltage = currentSensor.getMaxVoltage();
+        double voltage = currentSensor.getVoltage();
+        double current = 80 * (voltage / maxVoltage);
+        Logger.message("sensor value: %f  max: %f,  current: %f", voltage, maxVoltage, current );
+        return 80 * (voltage / maxVoltage);  // 80 amps is the max measured current
+    }
+}
---
+++ TeamCode/src/main/java/common/Hopper.java
@@ -0,0 +1,38 @@
+package common;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Hopper {
+
+    private final Servo lever;
+    private boolean open = false;
+    protected final double leverUpPosition = 0.7;
+    protected double levelDownPosition = 0.345;
+
+    public Hopper(LinearOpMode opMode) {
+        lever = opMode.hardwareMap.get(Servo.class, Config.LEVER);
+        leverUp();
+    }
+
+    public void leverDown() {
+        lever.setPosition(levelDownPosition);
+        open = true;
+    }
+
+    public void leverUp() {
+        lever.setPosition(leverUpPosition);
+        open = false;
+    }
+
+    /**
+     * Open the lever if it is closed, otherwise close it.
+     */
+    public void leverToggle() {
+        if (open) {
+            leverUp();
+        } else {
+            leverDown();
+        }
+    }
+}
